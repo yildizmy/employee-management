450a6cf8a3df236c7362604a84011dcb3f4fa67a
Murat Yıldız <murat.yildiz@kuehne-nagel.com>
2024-05-26 12:49:09 +0300

    add startup info for config-service

--- config-service/src/main/java/com/github/yildizmy/ConfigApplication.java
+++ config-service/src/main/java/com/github/yildizmy/ConfigApplication.java
@@ -1,13 +1,40 @@
 package com.github.yildizmy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j(topic = "ConfigApplication")
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+        SpringApplication app = new SpringApplication(ConfigApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = (env.getProperty("server.ssl.key-store") != null) ? "https" : "http";
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path", "/");
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n" +
+                "-------------------------------------------------------\n" +
+                "Application '" + env.getProperty("spring.application.name") + "' is running! Access URLs:\n" +
+                "Local:      " + protocol + "://localhost:" + serverPort + contextPath + "\n" +
+                "External:   " + protocol + "://" + hostAddress + ":" + serverPort + contextPath + "\n" +
+                "Profile(s): " + String.join(",", env.getActiveProfiles()) + "\n" +
+                "-------------------------------------------------------");
     }
 }
