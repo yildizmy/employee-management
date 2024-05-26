caa2fabdb8da8135dd09c839a4605c3eb2a8c907
Murat Yıldız <murat.yildiz@kuehne-nagel.com>
2024-05-26 11:12:16 +0300

    implement discovery-service

--- discovery-service/src/main/java/com/github/yildizmy/DiscoveryApplication.java
+++ discovery-service/src/main/java/com/github/yildizmy/DiscoveryApplication.java
@@ -2,12 +2,13 @@ package com.github.yildizmy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
-public class DiscoveryServiceApplication {
+public class DiscoveryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServiceApplication.class, args);
+        SpringApplication.run(DiscoveryApplication.class, args);
     }
-
 }
