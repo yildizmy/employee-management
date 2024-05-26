63885bae1efe9f6899b0751a9a0a6f82caea5c7d
Murat Yıldız <murat.yildiz@kuehne-nagel.com>
2024-05-26 12:49:37 +0300

    add startup info for department-service

--- department-service/src/main/java/com/github/yildizmy/DepartmentApplication.java
+++ department-service/src/main/java/com/github/yildizmy/DepartmentApplication.java
@@ -4,11 +4,17 @@ import com.github.yildizmy.model.Department;
 import com.github.yildizmy.repository.DepartmentRepository;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@Slf4j(topic = "DepartmentApplication")
 @SpringBootApplication
 @EnableFeignClients
 @OpenAPIDefinition(info =
@@ -17,7 +23,28 @@ import org.springframework.context.annotation.Bean;
 public class DepartmentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DepartmentApplication.class, args);
+        SpringApplication app = new SpringApplication(DepartmentApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = (env.getProperty("server.ssl.key-store") != null) ? "https" : "http";
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path", "/");
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n" +
+                "-------------------------------------------------------\n" +
+                "Application '" + env.getProperty("spring.application.name") + "' is running! Access URLs:\n" +
+                "Local:      " + protocol + "://localhost:" + serverPort + contextPath + "\n" +
+                "External:   " + protocol + "://" + hostAddress + ":" + serverPort + contextPath + "\n" +
+                "Profile(s): " + String.join(",", env.getActiveProfiles()) + "\n" +
+                "-------------------------------------------------------");
     }
 
     @Bean
