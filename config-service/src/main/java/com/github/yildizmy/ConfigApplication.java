ca8b619c38370a6882fcf34cabcaeba173a69554
Murat Yıldız <murat.yildiz@kuehne-nagel.com>
2024-05-27 15:00:48 +0300

    format code

--- config-service/src/main/java/com/github/yildizmy/ConfigApplication.java
+++ config-service/src/main/java/com/github/yildizmy/ConfigApplication.java
@@ -8,7 +8,6 @@ import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-
 @Slf4j(topic = "ConfigApplication")
 @EnableConfigServer
 @SpringBootApplication
